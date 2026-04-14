ca243b8d7910d6231f650bc39454141b25fab7be
Tran, Wilson <wdtran2018@gmail.com>
2026-04-14 01:44:12 -0700

    Added recommended function headers and include statements

--- frequencies.cpp
+++ frequencies.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+void getWords(vector<string>& words, ifstream& file);
+void sortWords(vector<string>& words);
+vector<pair<string, int>> getWordCounts(vector<string>& words);
 
 int main()
 {
